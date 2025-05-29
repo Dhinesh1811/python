5e04f3a5f7ace30eab88f06329de898bd0805023
Deepa0703 <deepapunniyakodi07@gmail.com>
2025-05-29 20:21:44 +0530

    Python Django project basic pages

---
+++ django_projects/Blob/myapp/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+import logging
+
+posts = [
+        {'id':1, 'title': 'post 1', 'content': 'Content of Post 1'},
+        {'id':2, 'title': 'post 2', 'content': 'Content of Post 2'},
+        {'id':3, 'title': 'post 3', 'content': 'Content of Post 3'},
+        {'id':4, 'title': 'post 4', 'content': 'Content of Post 4'}
+    ]
+
+# Create your views here.
+def index(request):
+    blog_title = "Latest Posts"
+    return render(request, 'blog\index.html', {'blog_title': blog_title, 'posts' : posts})
+
+def detail(request, post_id):
+     post = next((item for item in posts if item['id'] == int(post_id)), None)
+     #logger = logging.getLogger("Testing")
+     #logger.debug(f'Post Variable is {post}')
+     return render(request, 'blog\detail.html', {'post': post})
+
+def old_url_redirect(request):
+    return redirect(reverse('blog:new_page_url'))
+
+def new_url_view(request):
+    return HttpResponse("This is the new url")
---
+++ django_projects/Blob/readme.txt
@@ -0,0 +1,12 @@
+Steps to start the blob server:
+	
+Step 1: Activate the virtual environment:
+	Execute the "\python\django_projects\Blob\env\Scripts\activate" file to activate the virtual environment.
+
+Step 2. Start the server:
+	Navigate to "C:\Project\dhinesh\gitspace\python\django_projects\Blob\myapp".
+	Execute the below command:
+		python manage.py runserver
+
+Step 3: Access the Blob page(Our web application)
+	Launch "http://localhost:8000" to use the Blob app.
